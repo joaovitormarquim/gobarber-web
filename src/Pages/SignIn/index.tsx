6b038c45f6c7732e7c446d047d5b83124b993d90
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-07-19 22:58:20 -0300

    Remove toast automatically after three seconds

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -23,10 +23,8 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useAuth();
+  const { signIn } = useAuth();
   const { addToast } = useToast();
-
-  console.log(user);
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
